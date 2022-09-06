0de3296780b385293d16296630409e32c8210c0f
Thomas Musselman <tmusselman@gmail.com>
2022-09-06 17:04:51 -0400

    test: project#contestant_count

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -20,6 +20,16 @@ RSpec.describe Project, type: :model do
     @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
     @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
     @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
   end
 
   describe 'instance methods' do
@@ -27,6 +37,13 @@ RSpec.describe Project, type: :model do
       it 'returns the value of its challenge theme' do
         expect(@lit_fit.challenge_theme).to eq("Apartment Furnishings")
         expect(@boardfit.challenge_theme).to eq("Recycled Material")
+      end
+
+      it 'returns the number of contestants involved' do
+        expect(@lit_fit.contestant_count).to eq(0)
+        expect(@upholstery_tux.contestant_count).to eq(2)
+        expect(@boardfit.contestant_count).to eq(2)
+        expect(@news_chic.contestant_count).to eq(2)
       end
     end
   end
