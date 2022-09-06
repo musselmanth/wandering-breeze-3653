fdcf61a20e18bb6c40a6d55c3bbf96529ee5a22d
Thomas Musselman <tmusselman@gmail.com>
2022-09-06 17:09:03 -0400

    test: projects index inclues contestant count

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Project Show Page', type: :feature do
   before(:each) do
     @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-    @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
     @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-    @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
-    @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
-    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
   end
 
   it 'lists the project name, materials, and theme on the show page' do
@@ -16,5 +16,11 @@ RSpec.describe 'Project Show Page', type: :feature do
     expect(page).to have_content("News Chic")
     expect(page).to have_content("Material: Newspaper")
     expect(page).to have_content("Challenge Theme: Recycled Material")
+  end
+
+  it 'lists the number of contestants on the project' do
+    visit("/projects/#{@news_chic.id}")
+
+    expect(page).to have_content("Number of Contestants: 2")
   end
 end
